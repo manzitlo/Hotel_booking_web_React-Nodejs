88dbb004c81bf577e66a8713ad996597c81634af
Wenzhe Luo <hellolwz1120@gmail.com>
2023-01-20 12:14:18 -0500

    setting verifytoken & controller files

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,7 +1,19 @@
 import express from "express";
+import { createRoom } from "../controllers/roomController";
+import { verifyAdmin } from "../utils/verifyToken";
 
 const router = express.Router();
 
+//Create
+router.post("/", verifyAdmin, createRoom);
 
+//update
+router.put("/:id", verifyAdmin, updateRoom);
+//delete
+router.delete("/:id", verifyAdmin, deleteRoom);
+//get
+router.get("/:id", getRoom);
+//get all
+router.get("/", getRooms);
 
 export default router;
